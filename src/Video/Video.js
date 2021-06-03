75ba2a755c56a846c4365225179049b397d4d8e2
Myra Smith <61550209+myrasmith@users.noreply.github.com>
2021-06-03 12:38:26 -0400

    Update Video.js

--- src/Video/Video.js
+++ src/Video/Video.js
@@ -5,6 +5,11 @@ export class Video extends Component {
   render() {
     return (
       <div className="Video">
+      {/* 
+        try using the react-youtube package rather than creating your own iframe!
+        It's nice practice learning how to use 3rd party libraries and gives you an 
+        easy way to hook into the video player events (onplay, onpause, etc)
+      */}
         <iframe
           src={`https://www.youtube.com/embed/${this.props.match.params.id}`} //accesses id from /videos/:id via match.params on props
           allowFullScreen
