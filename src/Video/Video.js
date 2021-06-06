62721c61c11d812b953720bdcfc596a28a41f994
cassidybeni <cassidybeni@pursuit.org>
2021-06-06 13:55:35 -0400

    Use react-youtube instead of iframe

--- src/Video/Video.js
+++ src/Video/Video.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
+import YouTube from "react-youtube";
 import "./Video.css";
 
 export class Video extends Component {
   render() {
+    const opts = {
+      height: "315",
+      width: "560",
+      playerVars: {
+        autoplay: 1,
+      },
+    };
     return (
       <div className="Video">
-        <iframe
-          src={`https://www.youtube.com/embed/${this.props.match.params.id}`} //accesses id from /videos/:id via match.params on props
-          allowFullScreen
-          title="YTPlayer"
-        ></iframe>
+        <YouTube videoId={this.props.match.params.id} opts={opts}></YouTube>
       </div>
     );
   }
 }
 
 export default Video;
-
-// Reference:
-// https://www.npmjs.com/package/react-youtube
